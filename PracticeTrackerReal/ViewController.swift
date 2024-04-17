d178e8baefef1d85531786b9671810255b052af3
Mason Bartell <masonjbartell@gmail.com>
2024-04-17 09:20:45 -0500

    Initial Commit

---
+++ PracticeTrackerReal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeTrackerReal
+//
+//  Created by Mason  Bartell on 4/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
