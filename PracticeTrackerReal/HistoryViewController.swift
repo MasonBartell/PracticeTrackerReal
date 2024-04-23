5e6a4042007b3c8f1b16aacb864163b436351cfe
Mason Bartell <masonjbartell@gmail.com>
2024-04-23 09:03:51 -0500

    log New

--- PracticeTrackerReal/HistoryViewController.swift
+++ PracticeTrackerReal/HistoryViewController.swift
@@ -7,23 +7,26 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class HistoryViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var HistoryTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        HistoryTableView.dataSource = self
+        HistoryTableView.delegate = self
+       
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+  
 
 }
---
+++ PracticeTrackerReal/ViewController3.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController3.swift
+//  PracticeTrackerReal
+//
+//  Created by Mason  Bartell on 4/22/24.
+//
+
+import UIKit
+
+class AppData {
+    var count = 1
+}
+
+class ViewController3: UIViewController {
+
+    @IBOutlet weak var DrillsText: UITextField!
+    
+    @IBOutlet weak var goalText: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var dateText: UITextField!
+    
+    let dateFormatter = DateFormatter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+    dateText.inputView = datePicker
+        datePicker.datePickerMode = .date
+    dateText.text = dateFormatter.string(from: datePicker.date)
+    }
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        var dText = DrillsText.text!
+        var gText = goalText.text!
+        var dateT = dateText.text!
+        print("\(dText),\(gText),\(dateT)")
+    }
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+              
+            dateText.text = dateFormatter.string(from: sender.date)
+            view.endEditing(true)
+        }
+    
+}
